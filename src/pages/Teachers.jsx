42a31cc70a6fa34bf74610ee28ff8d8c26f2a3ec
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-10-24 16:05:23 +0500

    push

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -4,11 +4,5 @@ import axios from 'axios'
 import { Button } from 'antd'
 import { useNavigate } from 'react-router-dom'
 
-function Users() 
-	const navigate = useNavigate()
-	const [users, setUsers] = useState([])
-	useEffect() => {
-		
-}
 
 export default Users
